47a5db6f11ac81337e1440a022f5fab67dcdd4b2
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-04-01 12:27:14 +0530

    moved files from test to main

--- SeleniumBasics/src/main/java/com/expleo/SeleniumBasics/TestDemo.java
+++ SeleniumBasics/src/main/java/com/expleo/SeleniumBasics/TestDemo.java
@@ -1,13 +1,12 @@
 package com.expleo.SeleniumBasics;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Keys;
 
 public class TestDemo {
-	
 	public static void main(String args[]) throws InterruptedException {
 		WebDriver driver  = new ChromeDriver();
 		driver.manage().window().maximize();
---
+++ SeleniumBasics/src/main/java/com/expleo/SeleniumBasics/UsingDriverCommands.java
@@ -0,0 +1,27 @@
+package com.expleo.SeleniumBasics;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class UsingDriverCommands {
+
+	public static void main(String args[]) {
+
+		WebDriver driver = new ChromeDriver();
+
+		String url = "https://antigravityai.io/";
+		driver.get(url);
+
+		String title = driver.getTitle();
+		System.out.println("Title: " + title);
+		System.out.println("Title length: " + title.length());
+
+		System.out.println("Current Url: " + driver.getCurrentUrl());
+
+		String pageSource = driver.getPageSource();
+		System.out.println("PageSource Length: " + pageSource.length());
+
+		driver.quit();
+	}
+
+}
