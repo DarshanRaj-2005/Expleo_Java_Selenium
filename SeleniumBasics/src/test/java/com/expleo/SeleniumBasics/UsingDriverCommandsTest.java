18dc0679e6ab1bc880bf3abdfe4768435f411622
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-04-01 12:00:00 +0530

    Changed Program dependencies

--- SeleniumBasics/src/test/java/com/expleo/SeleniumBasics/UsingDriverCommandsTest.java
+++ SeleniumBasics/src/test/java/com/expleo/SeleniumBasics/UsingDriverCommandsTest.java
@@ -1,24 +1,35 @@
 package com.expleo.SeleniumBasics;
+
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class UsingDriverCommandsTest {
 
-	public static void main(String[] args) {
-		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		String url = "https://antigravityai.io/";
-		driver.get(url);
-		String title = driver.getTitle();
-		System.out.println("Title: "+title);
-		System.out.println("TItle length: "+title.length());
-		
-		System.out.println("Current Url: "+driver.getCurrentUrl());
-		String pageSource = driver.getPageSource();
-//		System.out.println("PageSource: "+pageSource);
-		System.out.println("PageSource Length: "+pageSource.length());
-		
-		driver.quit();
-	}
+    @Test
+    void runTest() {
 
+        // Headless setup
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");   // modern headless
+        options.addArguments("--disable-gpu");
+        options.addArguments("--window-size=1920,1080");
+
+        WebDriver driver = new ChromeDriver(options);
+
+        String url = "https://antigravityai.io/";
+        driver.get(url);
+
+        String title = driver.getTitle();
+        System.out.println("Title: " + title);
+        System.out.println("Title length: " + title.length());
+
+        System.out.println("Current Url: " + driver.getCurrentUrl());
+
+        String pageSource = driver.getPageSource();
+        System.out.println("PageSource Length: " + pageSource.length());
+
+        driver.quit();
+    }
 }
