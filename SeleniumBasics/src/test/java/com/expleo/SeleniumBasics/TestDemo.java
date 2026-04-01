408f2bef4d682ef444d296b275852b1909d3fa45
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-04-01 11:25:48 +0530

    java selenium basics program files added

---
+++ SeleniumBasics/src/test/java/com/expleo/SeleniumBasics/TestDemo.java
@@ -0,0 +1,20 @@
+package com.expleo.SeleniumBasics;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Keys;
+
+public class TestDemo {
+	
+	public static void main(String args[]) throws InterruptedException {
+		WebDriver driver  = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.co.in");
+		WebElement search = driver.findElement(By.name("q"));
+		search.sendKeys("selenium",Keys.ENTER);
+		System.out.println("Page title is: " + driver.getTitle());
+		driver.quit();
+	}
+}
