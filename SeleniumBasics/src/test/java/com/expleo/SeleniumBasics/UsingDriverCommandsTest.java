2bdea76b0d8b3b23d723964daecea2f4fceac355
DarshanRaj-2005 <dharshujayapal@gmail.com>
2026-04-01 11:50:42 +0530

    changed file name

--- SeleniumBasics/src/test/java/com/expleo/SeleniumBasics/UsingDriverCommandsTest.java
+++ SeleniumBasics/src/test/java/com/expleo/SeleniumBasics/UsingDriverCommandsTest.java
@@ -1,9 +1,8 @@
 package com.expleo.SeleniumBasics;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class UsingDriverCommands {
+public class UsingDriverCommandsTest {
 
 	public static void main(String[] args) {
 		WebDriver driver = new ChromeDriver();
